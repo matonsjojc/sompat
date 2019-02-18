5600730b7f8210644114f56b26eac0943a91b80f
matonsjojc <matevz.podlipnik@gmail.com>
2019-02-18 22:14:59 +0100

    prva dva modela

--- sompat_project/patients/models.py
+++ sompat_project/patients/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Patient(models.Model):
-    
+class Pacient(models.Model):
+    som_id = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.som_id
+
+class Aparat(models.Model):
+    model = models.CharField(max_length=128, unique=True)
+    sn = models.CharField(max_length=128, unique=True)
+    aktiven = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.sn
