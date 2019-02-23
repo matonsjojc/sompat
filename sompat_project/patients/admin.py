165055ba4fcf992dc57299eb35a057bdaa705d80
matonsjojc <matevz.podlipnik@gmail.com>
2019-02-23 10:27:56 +0100

    simple modeladmin za pacient in aparat

--- sompat_project/patients/admin.py
+++ sompat_project/patients/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from patients.models import Pacient, Aparat
 
-# Register your models here.
-admin.site.register(Pacient)
-admin.site.register(Aparat)
+class PacientAdmin(admin.ModelAdmin):
+    list_display = ['som_id']
+
+class AparatAdmin(admin.ModelAdmin):
+    list_display = ['sn', 'model', 'aktiven']
+
+
+
+
+admin.site.register(Pacient, PacientAdmin)
+admin.site.register(Aparat, AparatAdmin)
--- sompat_project/populate_patients.py
+++ sompat_project/populate_patients.py
@@ -7,6 +7,10 @@ django.setup()
 from patients.models import Pacient, Aparat
 
 def populate():
+
+    RES_IBREEZE_AUTO_CPAP = 'RES_IBREEZE_AUTO_CPAP'
+    LOEWENSTEIN_PRISMA_AUTO_CPAP = 'LOEWENSTEIN_PRISMA_AUTO_CPAP'
+
     pacienti = [
         {"som_id": "0001ab"},
         {"som_id": "0002cd"},
@@ -14,20 +18,26 @@ def populate():
 
     aparati = [
         {"sn": "111111",
-         "model": RES_IBREEZE_AUTO_CPAP},
-        {"sn": "222222",
          "model": LOEWENSTEIN_PRISMA_AUTO_CPAP},
+        {"sn": "222222",
+         "model": RES_IBREEZE_AUTO_CPAP},
     ]
 
-    for p in pacienti.items():
+    for p in pacienti:
         som_id = p["som_id"]
         add_pacient(som_id)
 
-    for a in aparati.items():
+    for a in aparati:
         model = a["model"]
         sn = a["sn"]
         add_aparat(model, sn)
-        
+
+    #print out pacienti and aparati we have added
+    for p in Pacient.objects.all():
+        print("pacient: ", p)
+    for a in Aparat.objects.all():
+        print("aparat: ", a)
+
 
 def add_pacient(som_id):
     p = Pacient.objects.get_or_create(som_id=som_id)[0]
@@ -43,6 +53,6 @@ def add_aparat(model, sn, aktiven=False):
 
 
 #start execution:
-if __name__ = '__main__':
+if __name__ == '__main__':
     print("Starting population script...")
     populate()
